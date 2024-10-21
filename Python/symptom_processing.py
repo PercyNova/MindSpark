4004c7422e7ee4ee2fe99baaccc29796e711dcba
Pride-Makamu <makamupride@gmail.com>
2024-10-21 21:33:48 +0200

    Update symptom_processing.py

---
+++ Python/symptom_processing.py
@@ -0,0 +1,28 @@
+#Import statements and initial setup
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import torch
+from transformers import AutoTokenizer, AutoModel
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+from fuzzywuzzy import fuzz, process
+import json
+import pyttsx3
+
+#Text-to-speech setup
+engine = pyttsx3.init()
+
+def init_tts():
+    engine = pyttsx3.init()
+    engine.setProperty('rate', 150)  
+    engine.setProperty('volume', 1) 
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[1].id)  
+    return engine
+
+def speak(text):
+    print(text)
+    engine.say(text)
+    engine.runAndWait()
