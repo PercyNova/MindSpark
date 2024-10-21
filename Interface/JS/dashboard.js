571dc956f16b072222f76eef9a0f78a6ceec4869
Pride-Makamu <makamupride@gmail.com>
2024-10-21 12:59:48 +0200

    Add files via upload

---
+++ Interface/JS/dashboard.js
@@ -0,0 +1,9 @@
+function showChat() {
+    document.querySelector('.chat-interface').style.display = 'block';
+    document.querySelector('.container').style.display = 'none';
+}
+
+function hideChat() {
+    document.querySelector('.chat-interface').style.display = 'none';
+    document.querySelector('.container').style.display = 'flex';
+}
